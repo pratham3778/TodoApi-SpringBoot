3b49c599379bcfcbcee291e966ad3c2b78445c13
pratham_tanpure <pratham3778@gmail.com>
2025-06-30 20:03:17 +0530

    Added delete and patch mapping

--- src/main/java/com/todoapi/TodoController.java
+++ src/main/java/com/todoapi/TodoController.java
@@ -1,14 +1,15 @@
 package com.todoapi;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/todo")
 public class TodoController {
     private static List<Todo> todoList;
 
@@ -19,15 +20,69 @@ public class TodoController {
 
     }
 
-    @GetMapping("/todo")
-    public List<Todo> getTodo() {
-        return todoList;
+    @GetMapping
+    public ResponseEntity<List<Todo>> getTodo() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(todoList);
     }
 
-    @PostMapping("/todo")
-    public Todo createTodo(@RequestBody Todo newTodo) {
+    @PostMapping
+    /*
+    We can use this annotation to set the status code : @ResponseStatus(HttpStatus.CREATED)
+     */
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo newTodo) {
         todoList.add(newTodo);
 
-        return newTodo;
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(newTodo);
+    }
+
+    @GetMapping("/{todoId}")
+    public ResponseEntity<?> getTodoById(@PathVariable Long todoId) {
+        for (Todo todo : todoList) {
+            if (todo.getId() == todoId) {
+                return ResponseEntity.ok(todo);
+            }
+        }
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("Todo not found !");
+    }
+
+    @DeleteMapping("/{todoId}")
+    public ResponseEntity<String> deleteTodoById(@PathVariable Long todoId) {
+        Iterator<Todo> iterator = todoList.iterator();
+
+        while (iterator.hasNext()) {
+            Todo todo = iterator.next();
+            if (todo.getId() == todoId) {
+                iterator.remove();
+                return ResponseEntity
+                        .ok("Todo Deleted Successfully");
+            }
+        }
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("Todo not found");
+    }
+
+    @PatchMapping("/{todoId}")
+    public ResponseEntity<String> updateTodo(@PathVariable Long todoId, @RequestBody Todo updatedTodo) {
+        for (Todo todo : todoList) {
+            if (todo.getId() == todoId) {
+                todo.setTitle(updatedTodo.getTitle());
+                todo.setCompleted(updatedTodo.isCompleted());
+                todo.setUserId(updatedTodo.getUserId());
+
+                return ResponseEntity
+                        .ok("Todo updated Successfully");
+            }
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("Todo not found");
     }
 }
