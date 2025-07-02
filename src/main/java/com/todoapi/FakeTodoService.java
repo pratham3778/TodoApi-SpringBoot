f5f97ad1233ef94cec9a163870c9fef6b55a0c93
Pratham Tanpure <140080210+pratham3778@users.noreply.github.com>
2025-07-02 20:55:40 +0530

    Update FakeTodoService.java

--- src/main/java/com/todoapi/FakeTodoService.java
+++ src/main/java/com/todoapi/FakeTodoService.java
@@ -3,7 +3,7 @@ package com.todoapi;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-//@Component // spring auto manage beans of TodoService
+//@Component // spring auto manage beans of TodoService 
 @Service //service layer handles business logic
 public class FakeTodoService implements TodoService {
 
